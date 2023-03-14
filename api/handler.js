7d171d2d76fa5fafa03e3849d48b53bffea537b7
jon <jonrose.webdev@yahoo.com>
2023-03-14 08:14:16 -0500

    deploy

--- api/handler.js
+++ api/handler.js
@@ -1,55 +1,55 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
-const axios = require('axios')
+// const express = require('express')
+// const cors = require('cors')
+// require('dotenv').config()
+// const axios = require('axios')
 
-const app = express()
+// const app = express()
 
-console.log('tst')
+// console.log('tst')
 
-app.use(
-  cors({
-    origin: '*',
-  })
-)
+// app.use(
+//   cors({
+//     origin: '*',
+//   })
+// )
 
-app.get('/api', (req, res) => {
-  res.setHeader('Content-Type', 'text/html')
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
-  res.send('hi wyd')
-})
+// app.get('/api', (req, res) => {
+//   res.setHeader('Content-Type', 'text/html')
+//   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
+//   res.send('hi wyd')
+// })
 
-app.get('/api/smart-floor-price', (req, res) => {
-  async function getSmartFloorPrice(contractAddress) {
-    try {
-      const config = {
-        method: 'GET',
-        headers: { Authorization: `Bearer ${process.env.YOUR_API_KEY}` },
-        url: `/api/smart-floor-price`,
-      }
-      const data = await axios(config).then((res) => res.data)
+// app.get('/api/smart-floor-price', (req, res) => {
+//   async function getSmartFloorPrice(contractAddress) {
+//     try {
+//       const config = {
+//         method: 'GET',
+//         headers: { Authorization: `Bearer ${process.env.YOUR_API_KEY}` },
+//         url: `/api/smart-floor-price`,
+//       }
+//       const data = await axios(config).then((res) => res.data)
 
-      res.json(data)
-    } catch (err) {
-      console.log(err)
-    }
-  }
+//       res.json(data)
+//     } catch (err) {
+//       console.log(err)
+//     }
+//   }
 
-  const contractAddress = req.query.contractAddress 
-  getSmartFloorPrice(contractAddress)
-})
+//   const contractAddress = req.query.contractAddress 
+//   getSmartFloorPrice(contractAddress)
+// })
 
-module.exports = app
+// module.exports = app
 
 
 
-// export default function handler(request, response) {
-//   return response.status(200).json({
-//     body: request.body,
-//     query: request.query,
-//     cookies: request.cookies,
-//     env: process.env.API_KEY,
-//   });
-// }
+export default function handler(request, response) {
+  return response.status(200).json({
+    body: request.body,
+    query: request.query,
+    cookies: request.cookies,
+    env: process.env.API_KEY,
+  });
+}
 
 // deploy
